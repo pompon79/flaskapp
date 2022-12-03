5cb87a4125576938fa3ca77dc8e5e719289ffca5
Peter <forteklapy@gmail.com>
2022-12-03 13:50:40 +0100

    prometheus monitoring test

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import requests
 from flask import Flask, render_template
+from prometheus_flask_exporter import PrometheusMetrics
 import socket
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello():
-    url = 'https://randomfox.ca/floof/'
-    r = requests.get(url)
-    obj = r.json()
-    random_pic = obj['image']
     myhost = socket.gethostname()
     host_ip = socket.gethostbyname(myhost)
     return render_template(
         "app.html",
         myhostname=myhost,
-        ip=host_ip,
-        pic=random_pic
+        ip=host_ip
     )
